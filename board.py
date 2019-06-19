feea8359e99a41125f1c448b687f788c6a0179f0
Dhaval Panchal <dhaval@Dhavals-MacBook-Pro.local>
2019-06-19 15:32:19 -0700

    fixed board file

---
+++ board.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Board:
+
+    def __init__(self):
+        self.board = []
+        for i in range(100):
+            x = random.randint(0, 10)
+            if x < 4:
+                self.board[i] = WaterTile()
+            elif 4 <= x < 8:
+                self.board[i] = LightTile()
+            else:
+                self.board[i] = EventTile()
+
+    def get_tile(self, i):
+        return self.board[i]
+
+    def change_season(self, season):
+        for i in self.board:
+            i.season = season
