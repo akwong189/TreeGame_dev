2be811d1e04b79ee92e61e5c582c0fd6298d1dc5
Alterac8 <awong189@calpoly.edu>
2019-06-21 15:41:14 -0700

    updates to tree classes

--- Tree/poplar.py
+++ Tree/poplar.py
@@ -3,6 +3,14 @@ from tree import Tree
 class Poplar(Tree):
 
     def __init__(self):
+        """Poplar Tree
+        Base max movement of 9
+        Base max health of 8
+        Init water level of 13
+        Init light level of 13
+        """
         super(Poplar, self).__init__()
         self.max_movement += 3
         self.max_health -= 2 
+        self.init_light += 3
+        self.init_water += 3
--- Tree/tree.py
+++ Tree/tree.py
@@ -1,18 +1,37 @@
 class Tree():
+    
     def __init__(self):
+        """Base Tree
+        Base max movement of 6
+        Base max health of 10
+        Init water level of 10
+        Init light level of 10
+        """
         self.max_movement = 6
         self.max_health = 10 
         self.init_water = 10
         self.init_light = 10
 
     def get_max_movement(self):
+        """Get max movement"""
         return self.max_movement
 
     def get_max_health(self):
+        """Get max health"""
         return self.max_health
 
-    def get_max_movement(self):
-        return self.max_movement
+    def get_init_water(self):
+        """Get init water level"""
+        return self.init_water
 
-    def get_max_health(self):
-        return self.max_health
+    def get_init_light(self):
+        """Get init light level"""
+        return self.init_light
+
+    def __str__(self):
+        """To string function"""
+        return "max movement: %s | max health: %s | water: %s | light: %s" % (self.max_movement, self.max_health, self.init_water, self.init_light)
+
+    def get_all(self):
+        """Returns all variables in the form of movement, health, light, and water"""
+        return (self.max_movement, self.max_health, self.init_light, self.init_water)
