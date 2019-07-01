dd8cb8dfecde9b61392a0ac702aa7839a073dc02
Alterac8 <awong189@calpoly.edu.com>
2019-07-01 13:18:43 -0700

    wip gui

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ import sys, pygame
 from pygame.locals import *
 from board import Board
 
+def draw_board(surface, board):
+    items = []
+    x = 0
+    y = 0
+
+    for index, tile in enumerate(board.board):
+        print("drawing board")
+        pygame.draw.rect(surface, tile.color, (x, y, 50, 50))
+        if index < 12:
+            x += 50
+        elif index < 23:
+            y += 50
+        elif index < 35:
+            x -= 50
+        else:
+            y -= 50
+
+def make_screens(screen):
+    canvas = pygame.Surface((800, 600))
+    board = pygame.Rect(0, 0, 400, 300)
+    player = pygame.Rect(400, 0, 400, 300)
+    info = pygame.Rect(0, 300, 400, 300)
+    
+    screen.blit(canvas, (0, 0), board)
+    screen.blit(canvas, (400, 0), player)
+    screen.blit(canvas, (0, 300), info)
+
+
 def main():
     x = 0
     y = 0
@@ -13,17 +41,22 @@ def main():
     board = Board(players)
 
     screen.fill(WHITE)
-    for index, tile in enumerate(board.board):
-        print("drawing board")
-        pygame.draw.rect(screen, tile.color, (x, y, 50, 50))
-        if index < 12:
-            x += 50
-        elif index < 23:
-            y += 50
-        elif index < 35:
-            x -= 50
-        else:
-            y -= 50
+    canvas = pygame.Surface((800, 600))
+    draw_board(canvas, board)
+    screen.blit(canvas, (0, 0))
+
+    # for index, tile in enumerate(board.board):
+    #     print("drawing board")
+    #     pygame.draw.rect(screen, tile.color, (x, y, 50, 50))
+    #     if index < 12:
+    #         x += 50
+    #     elif index < 23:
+    #         y += 50
+    #     elif index < 35:
+    #         x -= 50
+    #     else:
+    #         y -= 50
+    # make_screens(screen)
     
     while 1:
         for event in pygame.event.get():
